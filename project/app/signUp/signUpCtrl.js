b2e0d0fd858de226309ad8b2798291a401e1aaba
arielaleco <arielaleco@gmail.com>
2018-07-19 00:52:57 +0300

    add

--- project/app/signUp/signUpCtrl.js
+++ project/app/signUp/signUpCtrl.js
@@ -1,9 +1,9 @@
 app.controller("signUpCtrl", function ($scope , $location) {
 
-    $scope.registerUser() = function () {
-       
-        $location.path("/register" );
+    $scope.registerUser = function () {
         alert("here");
+       // $location.path("/register" );
+        
     }
 
 });
