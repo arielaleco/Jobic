64f0aea2463071aa4f7d955a650a81ec6e90f81c
arielaleco <arielaleco@gmail.com>
2018-07-30 11:52:04 +0300

    add chart

--- project/app/dashBoard/dashBoardCtrl.js
+++ project/app/dashBoard/dashBoardCtrl.js
@@ -9,7 +9,11 @@ app.controller("dashBoardCtrl", function ($scope , $location, chatService) {
         $log.error(error)
     });
 
-
-   
+    $scope.labels = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
+    $scope.series = ['Series A', 'Series B'];
+    $scope.data = [
+        [65, 59, 80, 81, 56, 55, 40],
+        [28, 48, 40, 19, 86, 27, 90]
+      ];
 
 });
