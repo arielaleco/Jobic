dd2b9d61f874a3d1f505fc55ece3de96f5742407
arielaleco <arielaleco@gmail.com>
2018-08-05 03:18:12 +0300

    try emil

--- app/jobProcess/jobProcessCtrl.js
+++ app/jobProcess/jobProcessCtrl.js
@@ -24,8 +24,30 @@ $scope.SendAndSaveCV = function(){
     var mail = 'mailto:{{$scope.newCV.email}}?subject=' + $scope.newCV.description +
                '&body=' + $scope.newCV.description;
     //  $window.open(mail);
+//--------------------------------------------------
+// $.ajax({
+//     type: "POST",
+//     url: "https://mandrillapp.com/api/1.0/messages/send.json",
+//     data: {
+//             'key': 'k-WxFhe95AY56u6uRo4EHw',
+//             'message': {
+//                          'from_email': 'arielaleco@gmail.com',
+//                          'to': [{
+//                                  'email': 'arielaleco@gmail.com',
+//                                  'name': 'Deepak',
+//                                  'type': 'to'
+//                                }],
+//                           'autotext': 'true',
+//                           'subject': 'Hacked Email',
+//                          'html': 'This is test Email'
+//                        }
+//            }
+// }).done(function(response) {
+//       console.log("mail sent :" + response); // if you're into that sorta thing
+// });	
 
 
+    //--------------------------------------------------
     // <script src="https://smtpjs.com/v2/smtp.js">
     // </script>
     // https://www.smtpjs.com/
