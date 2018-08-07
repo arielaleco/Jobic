33349b37ec5efc2e6af71b252d196ac2812c6213
arielaleco <arielaleco@gmail.com>
2018-08-07 13:53:51 +0300

    changes

--- app/SendCV/sendCVCtrl.js
+++ app/SendCV/sendCVCtrl.js
@@ -1,4 +1,4 @@
-app.controller("sendCVCtrl", function ($scope , $location , $window, sharedService) {
+app.controller("sendCVCtrl", function ($scope , $location , $window, sharedService, user) {
 
     $scope.cvSentArr = [];
     sharedService.fillcvSentsFile().then(function (cvSentArr) {
@@ -15,35 +15,8 @@ $scope.newCV.date = d;
 $scope.newCV.email="arielaleco@gmail.com";
 // $scope.newCV.date = d.toDateString();
 
-$scope.SendAndSaveCV = function(){
-    // validation check - confirmation 
-    
-    var template_params = {
-        email_to: $scope.newCV.email,        
-        email_from: "from_name_value",        
-        
-        message_subject:$scope.newCV.JobTitle+ " position at " + $scope.newCV.company,
-        message_content:$scope.newCV.coverLetterVersion.content
-        
-     }
-     
-    // take the cover letter content and place it
-    
-     
-     var service_id = "default_service";
-     var template_id = "jobic";
-     emailjs.send(service_id,template_id,template_params);
 
-
-
-    // var mail = 'mailto:{{$scope.newCV.email}}?subject=' + $scope.newCV.description +
-    //            '&body=' + $scope.newCV.description;
-    //  $window.open(mail);
-    
-    // send 
-
-    // save
-
+$scope.SaveCV = function(){
     sharedService.addNewSentCV($scope.newCV);
   
     // notify the user
@@ -51,7 +24,32 @@ $scope.SendAndSaveCV = function(){
     // move to card View
     $('[ data-target="#cardView"]').tab('show');
 
-// clen the fields from the last sent !
+    // clen the fields from the last sent !
+}
+
+$scope.SendAndSaveCV = function(){
+    // validation check - confirmation 
+    
+    var template_params = {
+        email_to: $scope.newCV.email,        
+        email_from: user.getActiveUser().email,        
+        
+        message_subject:$scope.newCV.JobTitle+ " position at " + $scope.newCV.company,
+        message_content:$scope.newCV.coverLetterVersion.content
+        
+     }     
+    // take the cover letter content and place it    
+    var service_id = "default_service";
+     var template_id = "jobic";
+     emailjs.send(service_id,template_id,template_params);
+     SaveCV();
+
+
+    // var mail = 'mailto:{{$scope.newCV.email}}?subject=' + $scope.newCV.description +
+    //            '&body=' + $scope.newCV.description;
+    //  $window.open(mail);    
+    
+    
 
     
 
