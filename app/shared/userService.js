91613a83eb5359d2acdbb5648fb4b070c646a82b
arielaleco <arielaleco@gmail.com>
2018-08-11 15:12:36 +0300

    fix users

--- app/shared/userService.js
+++ app/shared/userService.js
@@ -1,15 +1,19 @@
 
-app.factory('user', function($http, $q) {
+app.factory('user', function ($http, $q) {
 
-    //var activeUser = null;
-    var activeUser = new User({fname:"Ariela", lname:"Leiba Cohen", id:"1", email:"arielaleco@gmail.com", password:"123"});//null;
+    var activeUser = null;
+    //var activeUser = new User({ fname: "Ariela", lname: "Leiba Cohen", id: "1", email: "arielaleco@gmail.com", password: "123" , password: "123"});//null;
+
 
     function User(plainUser) {
         this.fname = plainUser.fname;
         this.lname = plainUser.lname;
         this.email = plainUser.email;
+        this.password = plainUser.password;
+        this.birthDate= plainUser.birthDate;
+        this.phone= plainUser.phone;
         this.id = plainUser.id;
-    } 
+    }
 
 
     function isLoggedIn() {
@@ -24,14 +28,14 @@ app.factory('user', function($http, $q) {
         var async = $q.defer();
 
         var loginURL = "https://my-json-server.typicode.com/arielaleco/Jobic/users?email=" + email + "&password=" + password;
-        $http.get(loginURL).then(function(response) {
+        $http.get(loginURL).then(function (response) {
             if (response.data.length > 0) {
                 activeUser = new User(response.data[0]);
                 async.resolve(activeUser);
             } else {
                 async.reject("invalid credentials");
             }
-        }, function(err) {
+        }, function (err) {
             async.reject(err);
         });
 
@@ -42,11 +46,29 @@ app.factory('user', function($http, $q) {
         return activeUser;
     }
 
+    function registerUser(signUpUser) {
+        if (activeUser == undefined) {
+            activeUser = new User(signUpUser);//null; 
+        }
+        else {
+            activeUser.fname = signUpUser.fname;
+            activeUser.lname = signUpUser.lname;
+            activeUser.email = signUpUser.email;
+            activeUser.password = signUpUser.password;
+            activeUser.birthDate= signUpUser.birthDate;
+            activeUser.phone= signUpUser.phone;
+        }
+        // activeUser = new User(signUpUser);//null;
+        //this.id = plainUser.id;
+
+    }
+
     return {
         login: login,
         isLoggedIn: isLoggedIn,
         logout: logout,
-        getActiveUser: getActiveUser
+        getActiveUser: getActiveUser,
+        registerUser: registerUser
     }
 
 
--- app/signUp/signUpCtrl.js
+++ app/signUp/signUpCtrl.js
@@ -1,15 +1,22 @@
-app.controller("signUpCtrl", function ($scope , $location) {
+app.controller("signUpCtrl", function ($scope , $location , user) {
 
     $scope.registerUser = function () {
    
-        
-        
+        // add use to list 
+        user.registerUser($scope.signUpUser)
+
         $location.path("/profile" );
         
     }
-    $scope.fName="";
-    $scope.lName="";
-    $scope.email="";
-    $scope.password="";
+    $scope.signUpUser={};
+    // $scope.signUpUser.fName="";    
+    // $scope.signUpUser.lname="";
+    // $scope.signUpUser.email="";
+    // $scope.signUpUser.password="";
+
+    // $scope.fName="";
+    // $scope.lName="";
+    // $scope.email="";
+    // $scope.password="";
    
 });
